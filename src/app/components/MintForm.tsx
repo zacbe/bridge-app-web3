de0d659a01c7cd275f1a528ec69f8cd6eed523ec
@zacbe <zacbegonzalez@gmail.com>
2024-08-10 12:04:34 -0500

    chore: add mintForm component

---
+++ src/app/components/MintForm.tsx
@@ -0,0 +1,100 @@
+
+'use client';
+
+import React, { useState } from 'react';
+import { submitForm } from '@/app/actions/form';
+import { useAuth } from '@/context/AuthContext';
+
+enum SupportedNetworks {
+  arbitrumSepolia = 'ARBITRUM',
+  optimismSepolia = 'OPTIMISM',
+}
+
+
+const MintForm = () => {
+  const [amount, setAmount] = useState('');
+  const [sourceNetwork, setSourceNetwork] = useState<SupportedNetworks>(SupportedNetworks.arbitrumSepolia);
+  const [destinationNetwork, setDestinationNetwork] = useState<SupportedNetworks>(SupportedNetworks.optimismSepolia);
+  const [error, setError] = useState<string | null>(null);
+  const { signer } = useAuth();
+
+  const handleSubmit = async (action: string) => {
+    setError(null);
+
+    if (!amount || !sourceNetwork || !destinationNetwork) {
+      alert('Please fill out all fields.');
+      return;
+    }
+
+    if (sourceNetwork === destinationNetwork) {
+      setError(`Bridge cannot be made between the same network.`);
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('amount', amount);
+    formData.append('sourceNetwork', sourceNetwork);
+    formData.append('destinationNetwork', destinationNetwork);
+
+    try {
+      if (!signer) {
+        throw new Error('Wallet is not connected');
+      }
+
+      await submitForm(formData, signer);
+      console.log('Form submitted successfully');
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      setError('An error occurred while submitting the form.');
+    }
+  };
+
+  return (
+    <div className=" bg-gray-900 flex flex-col items-center justify-center p-4">
+      <h1 className='text-white text-xl p-4'>Mint Tokens (admin only)</h1>
+      <div className="w-full max-w-md p-8 bg-gray-800 rounded-lg">
+        {error && <div className="mb-4 text-red-500">{error}</div>}
+        <div className="mb-4">
+          <label className="block text-white text-lg mb-2">Amount to Mint:</label>
+          <input
+            type="text"
+            value={amount}
+            onChange={(e) => {
+              const value = e.target.value;
+              // Check if the value is a valid integer and is non-negative
+              if (/^\d*$/.test(value)) {
+                setAmount(value);
+              }
+            }}
+            className="w-full p-2 text-white bg-gray-700 rounded-lg"
+            placeholder="Amount"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-white text-lg mb-2">From Chain:</label>
+          <select
+            value={sourceNetwork}
+            onChange={(e) => setSourceNetwork(e.target.value as SupportedNetworks)}
+            className="w-full p-2 text-white bg-gray-700 rounded-lg"
+          >
+            <option value="ARBITRUM">Arbitrum Sepolia</option>
+            <option value="OPTIMISM">Optimism Sepolia</option>
+          </select>
+        </div>
+
+        <div className="flex justify-between mt-4">
+          <button
+            type="button"
+            className="w-full mr-2 px-4 py-2 text-white bg-blue-500 rounded-lg"
+            onClick={() => handleSubmit('bridge')}
+          >
+            Mint Tokens
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MintForm;
